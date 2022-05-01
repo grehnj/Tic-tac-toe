6875ceeb6bb5109f9d9ef50cddd9194b78e3ae73
Joakim Grehn <joakim.grehn@gmail.com>
2022-05-01 16:09:14 +0200

    Added functionality to place marks.

--- main.py
+++ main.py
@@ -1,24 +1,54 @@
 import pygame
 import sys
+import random
+from settings import *
 
 # initialization
 pygame.init()
 screen = pygame.display.set_mode((426, 426))  # use tuple to set resolution
 pygame.display.set_caption("Joakim's Tic-tac-toe game")
-
 grid = pygame.image.load('graphics/grid.png')
-
+x_img = pygame.image.load('graphics/x.png')
+o_img = pygame.image.load('graphics/o.png')
 clock = pygame.time.Clock()
+
+
+def get_mouse_pos():
+    pos = pygame.mouse.get_pos()
+
+    for i in range(len(grid_pos)):
+        if grid_pos[i][0] < pos[0] < grid_pos[i][0] + square_size:
+            if grid_pos[i][1] < pos[1] < grid_pos[i][1] + square_size:
+                if x_positions.count(i) > 0 or o_positions.count(i) > 0:
+                    return
+
+                if random.randint(0, 1):
+                    x_positions.append(i)
+                else:
+                    o_positions.append(i)
+
+
+def draw_marks(position_list, image):
+    for i in position_list:
+        screen.blit(image, (grid_pos[i][0] + size_margin, grid_pos[i][1] + size_margin))
+
 
 # game loop
 while True:
+    screen.fill((246, 246, 212))  # fill background color
+    screen.blit(grid, (12, 12))
+
+    draw_marks(x_positions, x_img)
+    draw_marks(o_positions, o_img)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # if user clicks window close button
             pygame.quit()
             sys.exit()
-
-    screen.fill((246, 246, 212))  # fill background color
-    screen.blit(grid, (12, 12))
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_presses = pygame.mouse.get_pressed()
+            if mouse_presses[0]:
+                get_mouse_pos()
 
     pygame.display.update()
     clock.tick(30)  # cap fps at 30
---
+++ settings.py
@@ -0,0 +1,18 @@
+grid_pos = (  # left, top
+    (12, 12),
+    (150, 12),
+    (288, 12),
+    (12, 150),
+    (150, 150),
+    (288, 150),
+    (12, 288),
+    (150, 288),
+    (288, 288)
+)
+
+square_size = 126
+mark_size = 100
+size_margin = (square_size - mark_size) / 2
+
+x_positions = []
+o_positions = []
