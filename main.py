6394eab0babe94da5f7ebc510fcbdd6f0bbe20c9
Joakim Grehn <joakim.grehn@gmail.com>
2022-05-01 08:56:25 +0200

    Created graphics.

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import sys
 
 # initialization
 pygame.init()
-screen = pygame.display.set_mode((400, 400))  # use tuple to set resolution
+screen = pygame.display.set_mode((426, 426))  # use tuple to set resolution
 pygame.display.set_caption("Joakim's Tic-tac-toe game")
+
+grid = pygame.image.load('graphics/grid.png')
+
 clock = pygame.time.Clock()
 
 # game loop
@@ -15,6 +18,7 @@ while True:
             sys.exit()
 
     screen.fill((246, 246, 212))  # fill background color
+    screen.blit(grid, (12, 12))
 
     pygame.display.update()
     clock.tick(30)  # cap fps at 30
